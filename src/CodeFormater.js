9ff731da6a6b6face29583836e518cac8d00734d
NICOLAS Arnaud <arno06@gmail.com>
2015-12-23 10:05:21 +0100

    Modification du selecteur

--- src/CodeFormater.js
+++ src/CodeFormater.js
@@ -2,11 +2,11 @@ var CodeFormater =
 {
     init:function()
     {
-        document.querySelectorAll("*.PendingCodeFormater").forEach(CodeFormater.applyTo);
+        document.querySelectorAll("*.PendingCodeFormater, *[class^='language-']").forEach(CodeFormater.applyTo);
     }
 };
 CodeFormater.thesaurus = {
-    commentaires:[/(\/\*\*[a-z0-9\s\*\,\'\$\.\:\#\@\/éèà!\-=\(\)]{0,}\*\*\/)/gi, /(&lt;!--[a-z0-9\s,']{0,}--&gt;)/gi],
+    comments:[/(\/\*\*[a-z0-9\s\*\,\'\$\.\:\#\@\/éèà!\-=\(\)]{0,}\*\*\/)/gi, /(&lt;!--[a-z0-9\s,']{0,}--&gt;)/gi],
     api:[/(alert)(\(|\s)/gi,/(console)/g,/(log)/g,/\.(parent)/g, /(new\s)/g, /(parent)/g],
     string:[/(\"[a-z0-9\-\s:\/\.\_\|éàçâêîôè\+\=\&\?\;]*\")/gi],
     keyword:[/(else\s)/g,/(if(\s){0,}\()/g,/(function\s)/g,/(var\s)/g,/(\sArray[\s|\(])/g,/(return\s)/g,/(true|false)/g,/(\sextends\s)/g,/(\simplements\s)/g,/(class\s)/g,/(public\s)/g,/(private\s)/g],
@@ -15,7 +15,8 @@ CodeFormater.thesaurus = {
     xml:[/(&lt;[a-z:]+[0-9]*)/gi,/(&lt;[a-z:]+[0-9]*&gt;)/gi, /(&lt;\/[a-z:]+[0-9]*)/gi,/(\/&gt;)/gi, /(&gt;)/gi]};
 CodeFormater.applyTo = function (pElement)
 {
-    pElement.classList.remove("PendingCodeFormater");
+    if(pElement.classList.contains('PendingCodeFormater'))
+        pElement.classList.remove("PendingCodeFormater");
     var codeColored = pElement.innerHTML, family;
     codeColored = codeColored.replace(/\</g, "&lt;");
     codeColored = codeColored.replace(/\>/g, "&gt;");
