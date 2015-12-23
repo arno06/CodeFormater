009f8bf5f9066888ec56e505900b34e86f8e0655
NICOLAS Arnaud <arno06@gmail.com>
2015-12-23 09:28:00 +0100

    Espaces

--- src/CodeFormater.js
+++ src/CodeFormater.js
@@ -1,35 +1,35 @@
 var CodeFormater =
 {
-	init:function()
-	{
-		document.querySelectorAll("*.PendingCodeFormater").forEach(CodeFormater.applyTo);
-	}
+    init:function()
+    {
+        document.querySelectorAll("*.PendingCodeFormater").forEach(CodeFormater.applyTo);
+    }
 };
 CodeFormater.thesaurus = {
-		commentaires:[/(\/\*\*[a-z0-9\s\*\,\'\$\.\:\#\@\/éèà!\-=\(\)]{0,}\*\*\/)/gi, /(&lt;!--[a-z0-9\s,']{0,}--&gt;)/gi],
-		api:[/(alert)(\(|\s)/gi,/(console)/g,/(log)/g,/\.(parent)/g, /(new\s)/g, /(parent)/g],
-		string:[/(\"[a-z0-9\-\s:\/\.\_\|éàçâêîôè\+\=\&\?\;]*\")/gi],
-		keyword:[/(else\s)/g,/(if(\s){0,}\()/g,/(function\s)/g,/(var\s)/g,/(\sArray[\s|\(])/g,/(return\s)/g,/(true|false)/g,/(\sextends\s)/g,/(\simplements\s)/g,/(class\s)/g,/(public\s)/g,/(private\s)/g],
-		op:[/(\()/g,/(\))/g,/(\{)/g,/(\})/g,/(\])/g,/(\[)/g,/(\-&gt;)/g],
-		php:[/(\&lt;\?php)/g,/(\?\&gt;)/g,/(&lt;\?)/g],
-		xml:[/(&lt;[a-z:]+[0-9]*)/gi,/(&lt;[a-z:]+[0-9]*&gt;)/gi, /(&lt;\/[a-z:]+[0-9]*)/gi,/(\/&gt;)/gi, /(&gt;)/gi]};
+    commentaires:[/(\/\*\*[a-z0-9\s\*\,\'\$\.\:\#\@\/éèà!\-=\(\)]{0,}\*\*\/)/gi, /(&lt;!--[a-z0-9\s,']{0,}--&gt;)/gi],
+    api:[/(alert)(\(|\s)/gi,/(console)/g,/(log)/g,/\.(parent)/g, /(new\s)/g, /(parent)/g],
+    string:[/(\"[a-z0-9\-\s:\/\.\_\|éàçâêîôè\+\=\&\?\;]*\")/gi],
+    keyword:[/(else\s)/g,/(if(\s){0,}\()/g,/(function\s)/g,/(var\s)/g,/(\sArray[\s|\(])/g,/(return\s)/g,/(true|false)/g,/(\sextends\s)/g,/(\simplements\s)/g,/(class\s)/g,/(public\s)/g,/(private\s)/g],
+    op:[/(\()/g,/(\))/g,/(\{)/g,/(\})/g,/(\])/g,/(\[)/g,/(\-&gt;)/g],
+    php:[/(\&lt;\?php)/g,/(\?\&gt;)/g,/(&lt;\?)/g],
+    xml:[/(&lt;[a-z:]+[0-9]*)/gi,/(&lt;[a-z:]+[0-9]*&gt;)/gi, /(&lt;\/[a-z:]+[0-9]*)/gi,/(\/&gt;)/gi, /(&gt;)/gi]};
 CodeFormater.applyTo = function (pElement)
 {
-	pElement.classList.remove("PendingCodeFormater");
-	var codeColored = pElement.innerHTML, family;
-	codeColored = codeColored.replace(/\</g, "&lt;");
-	codeColored = codeColored.replace(/\>/g, "&gt;");
-	for(var i in CodeFormater.thesaurus)
-	{
+    pElement.classList.remove("PendingCodeFormater");
+    var codeColored = pElement.innerHTML, family;
+    codeColored = codeColored.replace(/\</g, "&lt;");
+    codeColored = codeColored.replace(/\>/g, "&gt;");
+    for(var i in CodeFormater.thesaurus)
+    {
         if(!CodeFormater.thesaurus.hasOwnProperty(i))
             continue;
-		family = CodeFormater.thesaurus[i];
-		for(var j=0; j<family.length; ++j)
-		{
-			codeColored = codeColored.replace(family[j],"<span class='M4"+i+"'>$1</span>");
-		}
-	}
-	pElement.innerHTML = codeColored;
+        family = CodeFormater.thesaurus[i];
+        for(var j=0; j<family.length; ++j)
+        {
+            codeColored = codeColored.replace(family[j],"<span class='M4"+i+"'>$1</span>");
+        }
+    }
+    pElement.innerHTML = codeColored;
 };
 
 window.addEventListener("load", CodeFormater.init, false);
