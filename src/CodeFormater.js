2e8b2c109f0db65e7964a6ae78a851303798c26b
NICOLAS Arnaud <arno06@gmail.com>
2015-12-23 09:23:49 +0100

    Correction de caractères

--- src/CodeFormater.js
+++ src/CodeFormater.js
@@ -6,9 +6,9 @@ var CodeFormater =
 	}
 };
 CodeFormater.thesaurus = {
-		commentaires:[/(\/\*\*[a-z0-9\s\*\,\'\$\.\:\#\@\/ÈË‡!\-=\(\)]{0,}\*\*\/)/gi, /(&lt;!--[a-z0-9\s,']{0,}--&gt;)/gi],
+		commentaires:[/(\/\*\*[a-z0-9\s\*\,\'\$\.\:\#\@\/√©√®√†!\-=\(\)]{0,}\*\*\/)/gi, /(&lt;!--[a-z0-9\s,']{0,}--&gt;)/gi],
 		api:[/(alert)(\(|\s)/gi,/(console)/g,/(log)/g,/\.(parent)/g, /(new\s)/g, /(parent)/g],
-		string:[/(\"[a-z0-9\-\s:\/\.\_\|È‡Á‚ÍÓÙË\+\=\&\?\;]*\")/gi],
+		string:[/(\"[a-z0-9\-\s:\/\.\_\|√©√†√ß√¢√™√Æ√¥√®\+\=\&\?\;]*\")/gi],
 		keyword:[/(else\s)/g,/(if(\s){0,}\()/g,/(function\s)/g,/(var\s)/g,/(\sArray[\s|\(])/g,/(return\s)/g,/(true|false)/g,/(\sextends\s)/g,/(\simplements\s)/g,/(class\s)/g,/(public\s)/g,/(private\s)/g],
 		op:[/(\()/g,/(\))/g,/(\{)/g,/(\})/g,/(\])/g,/(\[)/g,/(\-&gt;)/g],
 		php:[/(\&lt;\?php)/g,/(\?\&gt;)/g,/(&lt;\?)/g],
@@ -16,11 +16,13 @@ CodeFormater.thesaurus = {
 CodeFormater.applyTo = function (pElement)
 {
 	pElement.classList.remove("PendingCodeFormater");
-	var codeColored = pElement.innerHTML, family, element;
+	var codeColored = pElement.innerHTML, family;
 	codeColored = codeColored.replace(/\</g, "&lt;");
 	codeColored = codeColored.replace(/\>/g, "&gt;");
 	for(var i in CodeFormater.thesaurus)
 	{
+        if(!CodeFormater.thesaurus.hasOwnProperty(i))
+            continue;
 		family = CodeFormater.thesaurus[i];
 		for(var j=0; j<family.length; ++j)
 		{
